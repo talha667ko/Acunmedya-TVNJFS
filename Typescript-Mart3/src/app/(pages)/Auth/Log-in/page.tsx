af14ea4a8ede300d09d7580fe75c448de088e46a
talha667ko <talhatr29@gmail.com>
2025-04-30 23:59:10 +0300

    Login page need fixes

--- Typescript-Mart3/src/app/(pages)/Auth/Log-in/page.tsx
+++ Typescript-Mart3/src/app/(pages)/Auth/Log-in/page.tsx
@@ -1,15 +1,84 @@
+"use client";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import {Form, FormField, FormItem, FormLabel, FormControl, FormDescription, FormMessage } from "@/components/ui/form";
+import { zodResolver } from "@hookform/resolvers/zod";
 import Link from "next/link";
+import { Input } from "@/components/ui/input";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Button } from "@/components/ui/button";
 
+const formSchema = z.object({
+    email: z.string().email({ message: "Email invalide" }),
+    password: z.string().min(8, { message: "Le mot de passe doit contenir au moins 8 caractères" }).max(20, { message: "Le mot de passe ne doit pas dépasser 20 caractères" }),
+});
 export default function Login() {
+    // 1. Define your form.
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            email: "",
+            password: "",
+        },
+    })
+
+    // 2. Define a submit handler.
+    function onSubmit(values: z.infer<typeof formSchema>) {
+        // Do something with the form values.
+        // ✅ This will be type-safe and validated.
+        console.log(values)
+    }
     return (
-        <div className="flex flex-col justify-center items-center max-w-4xl w-full m-auto border-2 rounded-4xl mt-32 gap-1">
-                <h1 className="text-4xl font-bold">Connexion</h1>
-                <p className="">Bienvenue sur notre site !</p>
-                <input type="text" placeholder="Nom d'utilisateur" className="border-2 rounded-md p-2 mb-4"/>
-                <input type="password" placeholder="Mot de passe" className="border-2 rounded-md p-2 mb-4"/>
-                <button className="bg-blue-500 text-white rounded-md p-2 mb-4">Se Connecter</button>
+        <Card className="flex flex-col justify-center items-center max-w-4xl w-full m-auto border-4 mt-32 gap-1">
+            <CardHeader className="w-full m-auto">
+                <CardTitle className="text-4xl font-bold">Connexion</CardTitle>
+                <CardDescription className="text-lg">Bienvenue sur notre site !</CardDescription>
+            </CardHeader>
+            <CardContent>
+            <div className="flex flex-col gap-1 mb-6 w-full m-auto justify-center items-center">
+                <Form {...form}>
+                    <form onSubmit={form.handleSubmit(onSubmit)}>
+                    <div className="w-full">
+                            <FormField
+                                control={form.control}
+                                name="email"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>Email</FormLabel>
+                                        <FormControl>
+                                            <Input placeholder="example@gmail.com" {...field} />
+                                        </FormControl>
+                                        <FormDescription>
+                                        </FormDescription>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                        </div>
+                        <div className="">
+                            <FormField
+                                control={form.control}
+                                name="password"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>Password</FormLabel>
+                                        <FormControl>
+                                            <Input placeholder="Password" {...field} />
+                                        </FormControl>
+                                        <FormDescription>
+                                        </FormDescription>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                        </div>
+                <Button className="w-full">Se Connecter</Button>
+                </form>
+                </Form>
+                </div>
                 <p className="text-center">Vous n'avez pas de compte ? <Link href={"/Auth/Register"} className="text-blue-500">Créer un compte</Link></p>
-            </div>
-        
+
+            </CardContent>
+        </Card>
     );
 }
--- Typescript-Mart3/src/app/(pages)/Auth/Register/page.tsx
+++ Typescript-Mart3/src/app/(pages)/Auth/Register/page.tsx
@@ -1,9 +1,9 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { Card, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import Link from "next/link";
-import {z} from "zod";
+import { z } from "zod";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod"
 import {
@@ -14,114 +14,121 @@ import {
     FormItem,
     FormLabel,
     FormMessage,
-  } from "@/components/ui/form"
+} from "@/components/ui/form"
 
 const formSchema = z.object({
-    firstName: z.string().min(1, { message: "Le prénom est requis" }).max(20, { message: "Le prénom ne doit pas dépasser 20 caractères" }),
-    lastName: z.string().min(1, { message: "Le nom est requis" }).max(20, { message: "Le nom ne doit pas dépasser 20 caractères" }),
+    firstName: z.string().min(2, { message: "Le prénom est requis" }).max(20, { message: "Le prénom ne doit pas dépasser 20 caractères" }),
+    lastName: z.string().min(2, { message: "Le nom est requis" }).max(20, { message: "Le nom ne doit pas dépasser 20 caractères" }),
     email: z.string().email({ message: "Email invalide" }),
     password: z.string().min(8, { message: "Le mot de passe doit contenir au moins 8 caractères" }).max(20, { message: "Le mot de passe ne doit pas dépasser 20 caractères" }),
 });
-  
+
 export default function Register() {
     // 1. Define your form.
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
         defaultValues: {
-          firstName: "",
-          lastName: "",
+            firstName: "",
+            lastName: "",
             email: "",
             password: "",
         },
-      })
-     
-      // 2. Define a submit handler.
-      function onSubmit(values: z.infer<typeof formSchema>) {
+    })
+
+    // 2. Define a submit handler.
+    function onSubmit(values: z.infer<typeof formSchema>) {
         // Do something with the form values.
         // ✅ This will be type-safe and validated.
         console.log(values)
-      }
-  return (
+    }
+    return (
         <Card className="flex flex-col justify-center items-center max-w-4xl w-full m-auto border-4 mt-32 gap-1">
+            <CardHeader className="w-full m-auto mb-6">
             <CardTitle className="text-4xl font-bold">Créer un Compte</CardTitle>
-            <p className="">Bienvenue sur notre site !</p>
+            <p className="">Inscrivez-vous pour profiter de nos services.</p>
+            </CardHeader>
+            <CardContent>
             <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)}>
-        <div className="grid grid-cols-2 gap-3 mb-6 w-3/4 m-auto justify-center items-center">
-        <div className="col-span-1">
-        <FormField
-          control={form.control}
-          name="firstName"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>First Name</FormLabel>
-              <FormControl>
-                <Input placeholder="First Name" {...field} />
-              </FormControl>
-              <FormDescription>
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        </div>
-        <div className="col-span-1">
-        <FormField
-          control={form.control}
-          name="lastName"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Last Name</FormLabel>
-              <FormControl>
-                <Input placeholder="Last Name" {...field} />
-              </FormControl>
-              <FormDescription>
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        </div>
-        <div className="col-span-2">
-        <FormField
-          control={form.control}
-          name="email"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Email</FormLabel>
-              <FormControl>
-                <Input placeholder="example@gmail.com" {...field} />
-              </FormControl>
-              <FormDescription>
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        </div>
-        <div className="col-span-2">
-        <FormField
-          control={form.control}
-          name="password"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Password</FormLabel>
-              <FormControl>
-                <Input placeholder="Password" {...field} />
-              </FormControl>
-              <FormDescription>
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        </div>
-        </div>
-        <Button className="w-full">S'inscrire</Button>
-            </form>
-    </Form>
-            <p className="text-center">Vous avez déjà un compte ? <Link href={"/Auth/Log-in"} className="text-blue-500">Se connecter</Link></p>
-            <p className="text-center">En vous inscrivant, vous acceptez nos <Link href={"/Account/Terms"} className="text-blue-500">Conditions d'utilisation</Link> et notre <Link href={"/Account/Privacy"} className="text-blue-500">Politique de confidentialité</Link>.</p>
+                <form onSubmit={form.handleSubmit(onSubmit)}>
+                    <div className="grid grid-cols-2 gap-1 mb-6 w-full m-auto justify-center items-center">
+                        <div className="col-span-1">
+                            <FormField
+                                control={form.control}
+                                name="firstName"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>First Name</FormLabel>
+                                        <FormControl>
+                                            <Input placeholder="First Name" {...field} />
+                                        </FormControl>
+                                        <FormDescription>
+                                        </FormDescription>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                        </div>
+                        <div className="col-span-1">
+                            <FormField
+                                control={form.control}
+                                name="lastName"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>Last Name</FormLabel>
+                                        <FormControl>
+                                            <Input placeholder="Last Name" {...field} />
+                                        </FormControl>
+                                        <FormDescription>
+                                        </FormDescription>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                        </div>
+                        <div className="col-span-2">
+                            <FormField
+                                control={form.control}
+                                name="email"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>Email</FormLabel>
+                                        <FormControl>
+                                            <Input placeholder="example@gmail.com" {...field} />
+                                        </FormControl>
+                                        <FormDescription>
+                                        </FormDescription>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                        </div>
+                        <div className="col-span-2">
+                            <FormField
+                                control={form.control}
+                                name="password"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>Password</FormLabel>
+                                        <FormControl>
+                                            <Input placeholder="Password" {...field} />
+                                        </FormControl>
+                                        <FormDescription>
+                                        </FormDescription>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                        </div>
+                    </div>
+                    <Button className="w-full">S'inscrire</Button>
+                </form>
+            </Form>
+            </CardContent>
+            <p className="text-center">Vous avez déjà un compte ? <Link href={"/Auth/Log-in"} className="text-blue-500 hover:underline">Se connecter</Link></p>
+            <p className="text-center">En vous inscrivant, vous acceptez nos 
+                <Link href={"/Account/Terms"} className="text-blue-500 hover:underline">Conditions d'utilisation</Link> et notre 
+                <Link href={"/Account/Privacy"} className="text-blue-500 hover:underline">Politique de confidentialité</Link>.
+            </p>
         </Card>
-  );
+    );
 }
